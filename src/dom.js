c5336470932920e7e03095c620e0fc2434fc05c5
piaohe <1441997102@qq.com>
2021-02-07 23:03:44 +0800

    add

--- src/dom.js
+++ src/dom.js
@@ -75,5 +75,24 @@ window.dom = {
             return node.innerHTML
         }
     },
+    html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    }, html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    }, html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    }
 
 };
