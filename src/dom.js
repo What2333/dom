3361fc72858ef64fb8811e636216b87e0f11b68c
piaohe <1441997102@qq.com>
2021-03-22 21:53:41 +0800

    style

--- src/dom.js
+++ src/dom.js
@@ -75,6 +75,22 @@ window.dom = {
             return node.innerHTML
         }
     },
-
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            // dom.style(div, 'color', 'red')
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                // dom.style(div, 'color')
+                return node.style[name]
+            } else if (name instanceof Object) {
+                // dom.style(div, {color: 'red'})
+                const object = name
+                for (let key in object) {
+                    node.style[key] = object[key]
+                }
+            }
+        }
+    },
 
 };
